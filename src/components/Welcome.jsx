b086528689ba9281246eb65789dfba522c672598
Filip Andonovski <filip@coqtail.com>
2018-10-03 20:17:20 +0200

    Added intro text to Welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -11,6 +11,8 @@ class Welcome extends Component {
         <header className="Welcome-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Pup Quiz</h1>
+          <p> Test your knowledge of dog breeds and share and compare your scores with your friends. </p>
+          <p> Looking at all those photos of cute dogs is just a bonus </p>
         </header>
        <Link className="button" to={`/game`}>Start</Link>
       </div>
